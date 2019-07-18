a84c67a1f6d2139270169020dcc1451dba8ff439
Patrick-Wilton <u6050506@anu.edu.au>
2019-07-18 12:47:31 +1000

    1.0.0

--- battery_control_system.py
+++ battery_control_system.py
@@ -80,14 +80,24 @@ class Subscriber:
 class Control:
     def __init__(self):
 
-        self.temp = 0
-
+        # Control Settings and Initial Values
         self.time_interval = 5  # minutes
         self.time_interval_hour = 1 / (60 / self.time_interval)
         self.time_max_count = int(24 / self.time_interval_hour)
         self.time_count = 1
         self.bat_power = 0
+        self.control_interval = 1
+        self.control_time = False
+        self.solar_filtering = False
+        self.house_filtering = False
+        self.bat_filtering = True
 
+        # Sets up Kalman Filters
+        self.solar_filter = KalmanFilter(1, 0, 1, 0, 1, 0.05, 1)
+        self.house_filter = KalmanFilter(1, 0, 1, 700, 1, 0.05, 1)
+        self.battery_filter = KalmanFilter(1, 0, 1, 0, 1, 0.05, 1)
+
+        # Creates Internal Data Store
         self.data = dict()
         self.data["soc"] = []
         self.data["bat_power"] = []
@@ -100,29 +110,31 @@ class Control:
         plt.xlabel('Time (Hours)')
         plt.ylabel('Power (kW)')
 
+        # ZeroMQ Publishing
+        pub_port = "8093"
+        self.pub_topic = 0
+        pub_context = zmq.Context()
+        self.pub_socket = pub_context.socket(zmq.PUB)
+        self.pub_socket.bind("tcp://*:%s" % pub_port)
+
+        # Erases Text File Contents
         open("control_power_values.txt", "w+").close()
 
     def battery_control(self, bat, solar, house):
 
-        # Prints Readings
-        # print('NEW READING')
-        # print('SOC')
-        # print(bat)
-        # print('bat power')
-        # print(self.bat_power)
-        # print('solar')
-        # print(solar)
-        # print('house')
-        # print(house)
+        # Data Filtering
+        if self.solar_filtering:
+            self.solar_filter.step(0, solar)
+            solar = self.solar_filter.current_state()
+        if self.house_filtering:
+            self.house_filter.step(0, house)
+            house = self.house_filter.current_state()
 
         # Internal Data Store
         self.data_store(sub.bat_SOC, sub.solar_power, sub.house_power)
 
         # Text File Writing
         self.write_to_text(bat, solar, house)
-
-        # Data Filtering
-        self.kalman_filter()
 
         # Data Plotting
         if self.time_count == self.time_max_count:
@@ -143,11 +155,12 @@ class Control:
         self.bat_power = -grid + self.bat_power
         if (bat == 0 and self.bat_power < 0) or (bat == 100 and self.bat_power > 0):
             self.bat_power = 0
+        if self.bat_filtering:
+            self.battery_filter.step(0, self.bat_power)
+            self.bat_power = self.battery_filter.current_state()
 
-        return self.bat_power
-
-    def kalman_filter(self):
-        self.temp = 1
+        # Publishing
+        self.pub_socket.send_string("%d %d" % (self.pub_topic, self.bat_power))
 
     def write_to_text(self, bat, solar, house):
         file = open("control_power_values.txt", "a+")
@@ -165,18 +178,40 @@ class Control:
         self.data["house_power"].append(house)
 
 
+class KalmanFilter:
+    def __init__(self, process_dynamics, control_dynamics, measurement_dynamics, current_state_estimate,
+                 current_prob_estimate, process_covariance, measurement_covariance):
+
+        self.pro_dyn = process_dynamics
+        self.con_dyn = control_dynamics
+        self.meas_dyn = measurement_dynamics
+        self.curr_state = current_state_estimate
+        self.curr_prob = current_prob_estimate
+        self.pro_cov = process_covariance
+        self.meas_cov = measurement_covariance
+
+    def current_state(self):
+        return self.curr_state
+
+    def step(self, control_input, measurement):
+
+        # Prediction Calculations
+        predicted_state_estimate = self.pro_dyn * self.curr_state + self.con_dyn * control_input
+        predicted_prob_estimate = (self.pro_dyn * self.curr_prob) * self.pro_dyn + self.pro_cov
+
+        # Innovation Calculations
+        innovation = measurement - self.meas_dyn * predicted_state_estimate
+        innovation_covariance = self.meas_dyn * predicted_prob_estimate * self.meas_dyn + self.meas_cov
+
+        # Posterior Calculations
+        kalman_gain = predicted_prob_estimate * self.meas_dyn * 1 / float(innovation_covariance)
+        self.curr_state = predicted_state_estimate + kalman_gain * innovation
+
+        # Identity Matrix
+        self.curr_prob = (1 - kalman_gain * self.meas_dyn) * predicted_prob_estimate
+
+
 if __name__ == '__main__':
-
-    # Control System Settings
-    control_interval = 1
-    control_time = False
-
-    # ZeroMQ Publishing
-    pub_port = "8093"
-    pub_topic = 0
-    pub_context = zmq.Context()
-    pub_socket = pub_context.socket(zmq.PUB)
-    pub_socket.bind("tcp://*:%s" % pub_port)
 
     # Creates Control Class Instance
     control = Control()
@@ -188,19 +223,14 @@ if __name__ == '__main__':
     # MAIN LOOP
     print('Starting Control System')
     while True:
-        if control_time:
+        if control.control_time:
 
-            bat_power = control.battery_control(sub.bat_SOC, sub.solar_power, sub.house_power)
-
-            pub_socket.send_string("%d %d" % (pub_topic, bat_power))
-            time.sleep(control_interval)
+            control.battery_control(sub.bat_SOC, sub.solar_power, sub.house_power)
+            time.sleep(control.control_interval)
 
         elif sub.battery_read == 1 & sub.solar_read == 1 & sub.house_read == 1:
 
-            bat_power = control.battery_control(sub.bat_SOC, sub.solar_power, sub.house_power)
-
-            # Publishing
-            pub_socket.send_string("%d %d" % (pub_topic, bat_power))
+            control.battery_control(sub.bat_SOC, sub.solar_power, sub.house_power)
 
             # Reset Reads
             sub.battery_read = 0
